540fb226f80542bb69751d349d07a810d9d7d70b
Felix Oertel <f@oer.tel>
2011-09-18 17:05:19 +0200

    [+MODEL] add SQL for type model

--- ext_tables.sql
+++ ext_tables.sql
@@ -10,6 +10,7 @@ CREATE TABLE tx_vimeoconnector_domain_model_video (
 	thumbnail varchar(255) DEFAULT '' NOT NULL,
 
 	categories int(11) DEFAULT '0' NOT NULL,
+	type int(11) DEFAULT '0' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
@@ -34,8 +35,29 @@ CREATE TABLE tx_vimeoconnector_domain_model_category (
 	description text NOT NULL,
 
 	videos int(11) DEFAULT '0' NOT NULL,
-	parent int(11) DEFAULT '0' NOT NULL,
-	children int(11) DEFAULT '0' NOT NULL,
+
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
+	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	starttime int(11) unsigned DEFAULT '0' NOT NULL,
+	endtime int(11) unsigned DEFAULT '0' NOT NULL,
+
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l18n_parent int(11) DEFAULT '0' NOT NULL,
+	l18n_diffsource mediumblob NOT NULL,
+
+	PRIMARY KEY (uid),
+	KEY parent (pid),
+);
+
+CREATE TABLE tx_vimeoconnector_domain_model_type (
+	uid int(11) unsigned DEFAULT '0' NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+
+	title text NOT NULL,
+
+	videos int(11) DEFAULT '0' NOT NULL,
 
 	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
 	crdate int(11) unsigned DEFAULT '0' NOT NULL,
